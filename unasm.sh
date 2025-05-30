c6c103b62169600980830624486a60da66f9f9e3
magistermaks <magistermaks@darktree.net>
2025-05-30 18:45:53 +0200

    Make unasm.sh use 64 bit mode

--- unasm.sh
+++ unasm.sh
@@ -13,12 +13,12 @@ echo "_start:" >> $txt
 echo "$@" >> $txt
 
 echo "[1] nasm $txt"
-nasm -f elf32 $txt -o $obj
+nasm -f elf64 $txt -o $obj
 
 echo "[2] link $obj"
-ld -nostdlib -melf_i386 $obj -o $elf
+ld -nostdlib -melf_x86_64 $obj -o $elf
 strip $elf
 
 echo "[3] dump $elf"
 #readelf -lSWa $elf
-objdump -Mintel -xD $elf
+objdump -Mintel,intel64 -xD $elf

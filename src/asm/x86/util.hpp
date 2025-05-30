ec3db588beb7d951a067b1c32650a407b59bc419
magistermaks <magistermaks@darktree.net>
2025-05-31 00:45:35 +0200

    Updated symbol maps

--- src/asm/x86/util.hpp
+++ src/asm/x86/util.hpp
@@ -6,7 +6,5 @@
 #define RETURN_TRANSIENT(T, format) {volatile T tmp; asm("" : format (tmp)); return tmp;}
 
 extern "C" {
-	extern int x86_check_mode();
 	extern int x86_call_into(uint32_t (*)());
-	extern int x86_switch_mode(uint32_t (*)());
 }

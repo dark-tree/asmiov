895b9e4a2332df672bfe4b72a2d6a13720cec214
magistermaks <magistermaks@darktree.net>
2025-12-14 12:20:52 +0100

    Add input support to test::call_shell()

--- test/test.hpp
+++ test/test.hpp
@@ -4,19 +4,51 @@
 
 namespace test {
 
-	inline std::string call_shell(std::string cmd) {
-		std::string out;
-		char buf[256];
+	inline std::string call_shell(std::string cmd, const std::string& input = "") {
 
-		cmd += " 2>&1";
-		FILE* pipe = popen(cmd.c_str(), "r");
-		if (!pipe) return out;
+		// refers to the stdin/stdout of the child process
+		// [0] read, [1] write
+		int pipe_stdin[2];
+		int pipe_stdout[2];
 
-		while (fgets(buf, sizeof(buf), pipe)) {
-			out += buf;
+		pipe2(pipe_stdin, 0);
+		pipe2(pipe_stdout, 0);
+
+		pid_t pid = fork();
+
+		if (pid == 0) {
+			dup2(pipe_stdin[0], STDIN_FILENO);
+			dup2(pipe_stdout[1], STDOUT_FILENO);
+			dup2(pipe_stdout[1], STDERR_FILENO);
+
+			// close the unused end
+			close(pipe_stdin[1]);
+			close(pipe_stdout[0]);
+
+			execl("/bin/sh", "sh", "-c", cmd.c_str(), nullptr);
+			exit(1);
 		}
 
-		pclose(pipe);
+		// close the unused end
+		close(pipe_stdin[0]);
+		close(pipe_stdout[1]);
+
+		if (!input.empty()) {
+			write(pipe_stdin[1], input.data(), input.size());
+		}
+
+		close(pipe_stdin[1]);
+
+		std::string out;
+		char buf[256];
+		ssize_t n;
+
+		while ((n = read(pipe_stdout[0], buf, sizeof(buf))) > 0) {
+			out.append(buf, n);
+		}
+
+		close(pipe_stdout[0]);
+		waitpid(pid, nullptr, 0);
 		return out;
 	}
 

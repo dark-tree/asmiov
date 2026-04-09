8fe8232dc201ba698025fe2aa0dd30a060555862
magistermaks <magistermaks@darktree.net>
2026-04-09 08:05:19 +0200

    New debug dump function

--- test/test.hpp
+++ test/test.hpp
@@ -54,4 +54,48 @@ namespace test {
 		return out;
 	}
 
+	inline void dump(asmio::SegmentedBuffer& buffer) {
+
+		if (buffer.elf_machine == asmio::ElfMachine::NONE) {
+			buffer.elf_machine = asmio::ElfMachine::NATIVE;
+		}
+
+		asmio::ObjectFile baked = asmio::to_elf(buffer, asmio::Label::UNSET).bake();
+		asmio::util::TempFile temp {baked};
+
+		std::string out = call_shell("objdump --visualize-jumps -wxd -Mintel " + temp.path());
+		printf("%s\n", out.c_str());
+
+		printf("\nAuto-generated assertions:\n\n");
+		printf("\tbuffer.link(0);\n");
+
+		int i = 0;
+
+		for (auto& segment : buffer.segments()) {
+			int count = segment.buffer.size();
+
+			if (count == 0) {
+				i ++;
+				continue;
+			}
+
+			count --;
+
+			printf("\tstd::vector<uint8_t> s%d = {", i);
+
+			for (int j = 0; j <= count; j ++) {
+				printf("0x%02x", segment.buffer[j]);
+
+				if (count != j) {
+					printf(", ");
+				}
+			}
+
+			printf("};\n");
+			printf("\tCHECK(buffer.segments()[%d].buffer, s%d); // %s\n\n", i, i, segment.name.c_str());
+			i ++;
+		}
+
+	}
+
 }
